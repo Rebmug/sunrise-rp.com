420d254bd66d902b211b08c6fa0df01fa12405d4
Rebmug <limosrebmug@gmail.com>
2019-06-12 13:05:22 +0530

    Added login page

---
+++ app/inc/login.inc.php
@@ -0,0 +1,25 @@
+                            <!--Login form-->
+                            <div class="col-md-8">
+                                <div class="login-box">
+                                    <div class="login-box-area">
+                                        <div class="login-box-header">
+                                            Login
+                                        </div>
+                                        <hr>
+                                        <div class="login-box-body">
+                                            <form>
+                                                <div class="form-group">
+                                                    <label for="Login-Username">Username:</label>
+                                                    <input type="text" class="form-control" id="Login-Username" placeholder="Enter your name.">
+                                                    <small id="emailHelp" class="form-text text-muted">Please use your master account room.</small>
+                                                </div>
+                                                <div class="form-group">
+                                                    <label for="Login-Password">Password:</label>
+                                                    <input type="password" class="form-control" id="Login-Password" placeholder="Enter your password.">
+                                                </div>
+                                                <button type="submit" class="form-control btn btn-loginpg"><i class="fas fa-lock"></i> Login</button>
+                                            </form>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
